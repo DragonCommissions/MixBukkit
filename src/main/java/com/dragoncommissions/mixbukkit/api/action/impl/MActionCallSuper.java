27a07e350f412dcff3c216ae89f0a297d3fdf964
fan87 <fan87.TW@gmail.com>
2022-03-14 22:47:47 +0800

    Fixed call super

--- src/main/java/com/dragoncommissions/mixbukkit/api/action/impl/MActionCallSuper.java
+++ src/main/java/com/dragoncommissions/mixbukkit/api/action/impl/MActionCallSuper.java
@@ -1,17 +1,15 @@
 package com.dragoncommissions.mixbukkit.api.action.impl;
 
 import com.dragoncommissions.mixbukkit.api.MixinPlugin;
-import com.dragoncommissions.mixbukkit.api.ObfMap;
 import com.dragoncommissions.mixbukkit.api.action.MixinAction;
+import com.dragoncommissions.mixbukkit.api.shellcode.impl.inner.IShellCodeMethodInvoke;
 import com.dragoncommissions.mixbukkit.utils.ASMUtils;
-import javassist.bytecode.Descriptor;
 import javassist.bytecode.Opcode;
-import net.minecraft.world.damagesource.DamageSource;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class MActionCallSuper implements MixinAction {
 
@@ -49,12 +47,15 @@ public class MActionCallSuper implements MixinAction {
             throw new IllegalArgumentException("Could not find super method in " + owner.getSimpleName());
         }
         method.instructions.clear();
-        method.instructions.add(ASMUtils.loadVar(Object.class, 0));
-        method.instructions.add(ASMUtils.loadVar(DamageSource.class, 1));
-        method.instructions.add(ASMUtils.loadVar(float.class, 2));
-        String descriptor = ASMUtils.getDescriptor(boolean.class, DamageSource.class, float.class);
-        String mappingName = plugin.getObfMap().resolveMapping(new ObfMap.MethodMapping(superMethod.getDeclaringClass().getName().replace(".", "/"), descriptor, method.name));
-        method.instructions.add(new MethodInsnNode(Opcode.INVOKESPECIAL, superMethod.getDeclaringClass().getName().replace(".", "/"), mappingName, descriptor));
+        int varNum = 0;
+        if (Modifier.isStatic(superMethod.getModifiers())) {
+            method.instructions.add(new VarInsnNode(Opcode.ALOAD, varNum++));
+        }
+        Class<?>[] parameterTypes = superMethod.getParameterTypes();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            method.instructions.add(ASMUtils.loadVar(parameterTypes[i], varNum++));
+        }
+        method.instructions.add(new IShellCodeMethodInvoke(superMethod).generate());
         method.instructions.add(ASMUtils.genReturnNode(superMethod.getReturnType()));
 
     }
