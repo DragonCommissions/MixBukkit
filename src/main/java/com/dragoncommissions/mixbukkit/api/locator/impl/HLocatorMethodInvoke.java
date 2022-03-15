c3476a61f8dfc328a6df6a9a13882b8dc8bf03f4
fan87 <fan87.TW@gmail.com>
2022-03-15 17:03:10 +0800

    Fixed MethodInvoke

--- src/main/java/com/dragoncommissions/mixbukkit/api/locator/impl/HLocatorMethodInvoke.java
+++ src/main/java/com/dragoncommissions/mixbukkit/api/locator/impl/HLocatorMethodInvoke.java
@@ -3,6 +3,7 @@ package com.dragoncommissions.mixbukkit.api.locator.impl;
 import com.dragoncommissions.mixbukkit.api.locator.HookLocator;
 import com.dragoncommissions.mixbukkit.utils.ASMUtils;
 import com.dragoncommissions.mixbukkit.utils.PostPreState;
+import com.google.common.base.Predicate;
 import lombok.AllArgsConstructor;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.MethodInsnNode;
@@ -12,23 +13,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-@AllArgsConstructor
 public class HLocatorMethodInvoke implements HookLocator {
 
-    private Method method;
+    private String owner;
+    private String desc;
+    private String name;
     private PostPreState state;
     private Predicate<Integer> filter;
+
+    public HLocatorMethodInvoke(Method method, PostPreState state, Predicate<Integer> filter) {
+        owner = method.getDeclaringClass().getName().replace(".", "/");
+        desc = ASMUtils.getDescriptor(method.getReturnType(), method.getParameterTypes());
+        name = method.getName();
+    }
+
+    public HLocatorMethodInvoke(Class<?> owner, Method method, PostPreState state, Predicate<Integer> filter) {
+        this.owner = owner.getName().replace(".", "/");
+        desc = ASMUtils.getDescriptor(method.getReturnType(), method.getParameterTypes());
+        name = method.getName();
+    }
 
     @Override
     public List<Integer> getLineNumber(InsnList insnList) {
         int amount = 0;
-        String owner = method.getDeclaringClass().getName().replace(".", "/");
-        String desc = ASMUtils.getDescriptor(method.getReturnType(), method.getParameterTypes());
         List<Integer> out = new ArrayList<>();
         for (int i = 0; i < insnList.size(); i++) {
             if (insnList.get(i) instanceof MethodInsnNode) {
                 MethodInsnNode insnNode = (MethodInsnNode) insnList.get(i);
-                if (insnNode.owner.equals(owner) && insnNode.name.equals(method.getName()) && insnNode.desc.equals(desc) && filter.test(amount++)) {
+                if (insnNode.owner.equals(owner) && insnNode.name.equals(name) && insnNode.desc.equals(desc) && filter.test(amount++)) {
                     out.add(i + (state == PostPreState.POST?1:0));
                 }
             }
